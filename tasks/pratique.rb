41562a38377f6d46b4fffb7be0057a1abd7d107b
Sebastien Badal-Badalian <59803115+sebio33@users.noreply.github.com>
2024-04-30 11:02:44 -0400

    Tache 09b - erreurs

---
+++ tasks/pratique.rb
@@ -0,0 +1,50 @@
+require_relative "../karel/ur_robot"
+
+class Pratique < UrRobot
+
+	def move_5
+		move
+		move
+		move
+		move
+		move
+	end 
+
+    def turn_around
+    	turn_left
+    	turn_left
+    	
+    end
+
+	def mettre_5_beeper
+		put_beeper
+		put_beeper
+		put_beeper
+		put_beeper
+		put_beeper
+	end
+
+    def turn_right
+    	turn_left
+    	turn_left
+    	turn_left
+    	
+    end
+
+	def diagonale_droit
+		move
+		turn_right
+		move
+		turn_left
+	end
+
+	def diagonale_gauche
+		move
+		turn_left
+		move
+		turn_right		
+	end
+
+
+end
+
